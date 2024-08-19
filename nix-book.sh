fb745bb73189659ca5bb0272f7b9c43c0ceac03a
meator <meator.dev@gmail.com>
2024-08-19 15:08:15 +0200

    Remove shell.nix
    This change removes support for entering development shell. It isn't
    that useful in this case and it still can be done if you try hard enough
    (and if you read the header of nix-book.sh).

--- nix-book.sh
+++ nix-book.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash --pure
+#! nix-shell -p bash mdbook mdbook-toc mdbook-admonish
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/tarball/nixos-24.05
 
 exec ./book.sh "$@"
